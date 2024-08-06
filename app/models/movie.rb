27dab20821d8a40c3c552b304913c213c7c5bf7d
nhatminhdao <nhatminhdao@gmail.com>
2024-08-06 19:03:47 +0000

    delete and generate again

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,2 @@
-# == Schema Information
-#
-# Table name: movies
-#
-#  id          :bigint           not null, primary key
-#  description :text
-#  title       :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  director_id :integer
-#
 class Movie < ApplicationRecord
 end
--- db/migrate/20240806190321_create_movies.rb
+++ db/migrate/20240806190321_create_movies.rb
@@ -2,7 +2,10 @@ class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
       t.string :title
+      t.integer :year
+      t.integer :duration
       t.text :description
+      t.string :image
       t.integer :director_id
 
       t.timestamps
