aae96800d06bc91ba9b67071a13ae92f0d53b0eb
nhatminhdao <nhatminhdao@gmail.com>
2024-08-06 19:16:38 +0000

    error

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_184055) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_191155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "actor_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
+    t.integer "year"
+    t.integer "duration"
     t.text "description"
+    t.string "image"
     t.integer "director_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
