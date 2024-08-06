055d36e465b810c9a258f98bf6a18960b7a99d86
nhatminhdao <nhatminhdao@gmail.com>
2024-08-06 18:28:47 +0000

    method 1

---
+++ app/models/movie.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  image_url   :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+end
